9a670dd89bbdff0aa211b22cad45afa29e38cd9c
shashikant kumar <shashi94goswami@gmail.com>
2025-07-10 22:25:05 +0530

    added netwwok

---
+++ learning-docker-network/app/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get("/",(req,res)=>{
+  res.send("Hello, Docker Network!");
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
